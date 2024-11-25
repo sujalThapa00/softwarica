3706108c0206788348b4263088c81e53641fd2cd
sujalThapa00 <thapasujal2063@gmail.com>
2024-11-25 08:14:39 +0545

    first commit

---
+++ models/Student.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication1.models
+{
+    public class Student
+    {
+        public int STUDENTID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int PhoneNumber { get; set; }
+        public DateTime Enrollmentdate { get; set; }
+        public string Address { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string CreatedBy  { get; set; }
+    }
+}
